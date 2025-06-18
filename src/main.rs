14e272f42e3119352042779c790c2b78fc7a320a
Jonathan Schmittat <194770151+HQ2000-Rust@users.noreply.github.com>
2025-06-18 19:38:21 +0200

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,87 @@
 use crate::utils::field_utils::{Field, Number, PlayingField};
-use std::io::{Read, Write};
+use crate::utils::general::get_input;
+use crate::utils::general::menu::settings::Flags;
 
+mod playing_field_templates;
 mod utils;
 
 fn main() {
     println!("Welcome to Terminal Sudoku!");
-
-    /*let field=PlayingField::from([[Field::Empty;9],
-        [Field::Empty;9],
-        [Field::Number(Number::Seven);9],
-        [Field::Empty;9],
-        [Field::Number(Number::Nine);9],
-        [Field::Empty;9],
-        [Field::Empty;9],
-        [Field::Empty;9],
-        [Field::Empty;9],]);
-    field.print();*/
-
-    game_loop();
+    let settings = crate::utils::general::menu::settings::get_raw_flags();
+    let settings = Flags {
+        stopwatch: settings.contains(&("stopwatch".to_string())),
+        sudoku_maker: settings.contains(&("sudoku_maker".to_string())),
+        templates: settings.contains(&("templates".to_string())),
+    };
+    game_loop(settings);
 }
 
 #[inline]
-fn game_loop() {
+fn game_loop(settings: Flags) {
     use utils::field_utils::{Field, PlayingField};
-    loop {
+    'round: loop {
         utils::general::menu::general_menu();
         let mut playing_field = PlayingField::new();
+        //TODO!: timer
+        loop {
+            println!("The field:");
+            playing_field.print();
+            println!("Which field do you want to change?");
+            let mut y_coord_switch = true;
+            let mut x_coord: usize = 0;
+            let mut y_coord: usize = 0;
+            'coords: loop {
+                println!("Input the {}-coordinate:", {
+                    if y_coord_switch { "y" } else { "x" }
+                });
+                match get_input().trim().parse::<usize>() {
+                    Err(_) => {
+                        println!("Please enter a number between 1 and 9 (inclusive)!");
+                        continue 'coords;
+                    }
+                    Ok(number) => {
+                        if !(number >= 1 && number <= 9) {
+                            println!("Please enter a number between 1 and 9 (inclusive)!");
+                            continue 'coords;
+                        }
+                        match y_coord_switch {
+                            true => {
+                                y_coord = number;
+                            }
+                            false => {
+                                x_coord = number;
+                            }
+                        }
+                        if !y_coord_switch {
+                            let field = playing_field.access(x_coord, y_coord);
+                            if !field.eq(&Field::Empty) {
+                                println!("You need an empty Field to put something in!");
+                                y_coord_switch = true;
+                                continue 'coords;
+                            }
+                            break 'coords;
+                        }
+                        y_coord_switch = false;
+                    }
+                }
+            }
+            println!("y: {y_coord}, x: {x_coord}");
+            let field_type_i32 = loop {
+                println!("Which number should be inserted? Type it in and press ENTER.");
+                if let Ok(number) = get_input().trim().parse::<i32>() {
+                    if !(number >= 1 && number <= 9) {
+                        break number;
+                    }
+                }
+                println!("Please enter a number between 1 and 9 (inclusive)!");
+            };
+            playing_field.set(
+                x_coord,
+                y_coord,
+                Field::Number(crate::utils::field_utils::i32_to_Number(field_type_i32).expect({
+                    unreachable!()
+                })),
+            )
+        }
     }
 }
