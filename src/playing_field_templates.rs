52ca2d002c24f13e253cbf10c53c8fb421a5b66d
Jonathan Schmittat <194770151+HQ2000-Rust@users.noreply.github.com>
2025-06-19 14:38:37 +0200

    Update playing_field_templates.rs

--- src/playing_field_templates.rs
+++ src/playing_field_templates.rs
@@ -1,16 +1,22 @@
 use crate::utils::field_utils::PlayingField;
 
-fn get_templates() -> Vec<crate::utils::field_utils::PlayingField> {
+//can be used if you want to load a standard config or add your own one
+pub fn get_template(number: u32) -> Vec<crate::utils::field_utils::PlayingField> {
+    #[allow(unused_imports)]
     use crate::{Field, Number};
-    vec![PlayingField::from([
-        [Field::Empty; 9],
-        [Field::Empty; 9],
-        [Field::Empty; 9],
-        [Field::Empty; 9],
-        [Field::Empty; 9],
-        [Field::Empty; 9],
-        [Field::Empty; 9],
-        [Field::Empty; 9],
-        [Field::Empty; 9],
-    ])]
+    use crate::{Field as F, Number as N};
+    //space for templates
+    match number {
+        _ => vec![PlayingField::from([
+            [Field::Empty; 9],
+            [Field::Empty; 9],
+            [Field::Empty; 9],
+            [Field::Empty; 9],
+            [Field::Empty; 9],
+            [Field::Empty; 9],
+            [Field::Empty; 9],
+            [Field::Empty; 9],
+            [Field::Empty; 9],
+        ])],
+    }
 }
